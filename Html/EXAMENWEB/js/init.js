ce791fddeaf054d197bb24f3b5b7de175b435d5d
gustavo gonzales davalos <ggonzales@viunex.com>
2013-06-28 19:10:11 -0500

    borrados php,viunex,html,xml,css

--- Html/EXAMENWEB/js/init.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-	$('#dropmenu').treeonic({
-		exclusive:	true,
-		expandOnHover: true,
-		collapseOnLeave: true							
-	});
-	
-});
--- PHP/PRACTICAS/Formularios/envia-datos.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if(isset($_GET["enviar_btn"])){
-		echo "Formulario enviado por metodo get,<br/> Los datos son:".$_GET["name_txt"]." y ".$_GET["psw_txt"];
-	}else if(isset($_POST["enviar_btn"])){
-		echo "Formulario enviado por metodo post,<br/> Los datos son:".$_POST["name_txt"]." y ".$_POST["psw_txt"];
-	}
-?>
